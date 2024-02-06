50093162b5fccdbc93f8cfd441f7603b9caf48a6
Marcio Angelino <37160293+mangelino997@users.noreply.github.com>
2024-02-06 02:21:21 -0300

    minot changes

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -1,27 +1,27 @@
 import { TABLE_NAME } from 'common/const';
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 
-const deleteTask = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
-  const { id } = event.pathParameters;
+const deleteTask = async event => {
+	const dynamodb = new AWS.DynamoDB.DocumentClient();
+	const { id } = event.pathParameters;
 
-  await dynamodb
-    .delete({
-      TableName: TABLE_NAME,
-      Key: {
-        id,
-      },
-    })
-    .promise();
+	await dynamodb
+		.delete({
+			TableName: TABLE_NAME,
+			Key: {
+				id,
+			},
+		})
+		.promise();
 
-  return {
-    status: 200,
-    body: {
-      message: 'Deleted Task'
-    }
-  };
+	return {
+		status: 200,
+		body: {
+			message: 'Deleted Task',
+		},
+	};
 };
 
 module.exports = {
-  deleteTask,
+	deleteTask,
 };
